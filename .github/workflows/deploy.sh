0aac4cca822a3f74bc60b52f0203433a246fbb6f
cdbbnny <cdbbnnycode@users.noreply.github.com>
2020-09-19 15:46:01 -0500

    Include dotfiles when copying output

--- .github/workflows/deploy.sh
+++ .github/workflows/deploy.sh
@@ -6,12 +6,12 @@ echo "Python: $(python3 --version)"
 user=$GITHUB_ACTOR
 pass=$GITHUB_TOKEN
 repo_url="https://$user:$pass@github.com/ftc-team-8813/website"
-git config user.name "$user"
-git config user.email "$user@users.noreply.github.com"
-echo "Committing as $user ($user@users.noreply.github.com)"
+# git config user.name "$user"
+# git config user.email "$user@users.noreply.github.com"
+# echo "Committing as $user ($user@users.noreply.github.com)"
 
 cd pages
-python3 builder.py || exit 1
+python3 builder.py -v || exit 1
 
 mv output/ ../../
 cd ..
@@ -22,8 +22,8 @@ git checkout deploy
 find -maxdepth 1 -type f -exec rm {} ';'
 find -maxdepth 1 -type d ! -path './.git' ! -path './.git/*' ! -path '.' -exec rm -r {} ';'
 
-# move everything back here
-mv ../output/* .
+# move everything back here INCLUDING dotfiles
+mv ../output/{*,.[^.],.??*} .
 rm -r ../output/
 
 # commit and push
